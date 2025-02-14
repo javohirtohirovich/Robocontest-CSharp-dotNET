06e1a0e8ad4dd3885164077364db06887a45a1d4
javohirtohirovich <javoergashev30@gmail.com>
2025-02-15 02:19:39 +0500

    Problem-1 solved

---
+++ Problems/Problem-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        Console.WriteLine(input[0] + input[1]);
+    }
+}
