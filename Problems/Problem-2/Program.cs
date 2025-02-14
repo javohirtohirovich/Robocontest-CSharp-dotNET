d4c0abe72c712d4b7deaa7dcc2dee146104e6ff2
javohirtohirovich <javoergashev30@gmail.com>
2025-02-15 02:27:17 +0500

    Problem-2 solved

---
+++ Problems/Problem-2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Problem_2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var input = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        if (input[0]>input[1])
+        {
+            Console.WriteLine('>');
+        }
+        else if (input[0]<input[1])
+        {
+            Console.WriteLine('<');
+        }
+        else
+        {
+            Console.WriteLine('=');
+        }
+    }
+}
