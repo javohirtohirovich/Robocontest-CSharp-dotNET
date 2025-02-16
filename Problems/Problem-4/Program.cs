8e4271b1cbf5d1857b736dcb4a3d3e2a08d899a8
javohirtohirovich <javoergashev30@gmail.com>
2025-02-17 01:40:58 +0500

    Problem-4 sloved

---
+++ Problems/Problem-4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Problem_4;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var inputValues = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+        Console.WriteLine(inputValues[0] * inputValues[1]);
+    }
+}
