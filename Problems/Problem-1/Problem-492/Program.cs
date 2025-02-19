c8831c6417b0674f0f23eca85b607bb0637826c5
javohirtohirovich <javoergashev30@gmail.com>
2025-02-19 19:05:03 +0500

    Probelem-492 resolved

---
+++ Problems/Problem-1/Problem-492/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_492;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine()!);
+        Console.WriteLine(1);
+    }
+}
