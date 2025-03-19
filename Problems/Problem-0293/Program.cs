e6fb7436101e809d69bd9ee7d2945c640f7d7c91
javohirtohirovich <javoergashev30@gmail.com>
2025-03-19 21:07:21 +0500

    Problems sloved

---
+++ Problems/Problem-0293/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Problem_0293;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var str = Console.ReadLine();
+        Console.WriteLine(str.Length);
+        foreach (var c in str)
+        {
+            string encryption = DecimalToBinary((int)c);
+            Console.WriteLine(encryption);
+        }
+    }
+
+    public static string DecimalToBinary(int number)
+    {
+        string binnary = "";
+        while (number > 0)
+        {
+            binnary = (number % 2) + binnary;
+            number = number / 2;
+        }
+
+        return binnary;
+    }
+}
---
+++ Problems/Problem-0376/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Problem_0376;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var n = int.Parse(Console.ReadLine());
+        if (n >= 3 && n <= 5)
+        {
+            Console.WriteLine("Spring");
+        }
+        else if (n >= 6 && n <= 8)
+        {
+            Console.WriteLine("Summer");
+        }
+        else if (n >= 9 && n <= 11)
+        {
+            Console.WriteLine("Autumn");
+        }
+        else if(n == 12 || n==1 || n == 2)
+        {
+            Console.WriteLine("Winter");
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+
+    }
+}
