1f278e1dca8cd85c6bab4c309ad9284b7511fdc1
javohirtohirovich <javoergashev30@gmail.com>
2025-03-20 11:15:09 +0500

    Problems sloved

---
+++ Problems/Problem-0229/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace Problem_0229;
+
+internal class Program
+{
+    static void Main()
+    {
+        var arrNums = Console.ReadLine().Split().Select(BigInteger.Parse).ToArray();
+
+        var arifmeticMean = (double)(arrNums[0] + arrNums[1]) / 2.0;
+        var geometricMean = Math.Sqrt((double)(arrNums[0] * arrNums[1]));
+
+        if (arifmeticMean > geometricMean)
+        {
+            Console.WriteLine(">");
+        }
+        else if (arifmeticMean < geometricMean)
+        {
+            Console.WriteLine("<");
+        }
+        else
+        {
+            Console.WriteLine("=");
+        }
+    }
+}
---
+++ Problems/Problem-0265/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_0265;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var arrNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.WriteLine(arrNums[0] * arrNums[1]);
+    }
+}
