8aa53b012fbfed0bb2b63ca708976961dae4325a
javohirtohirovich <javoergashev30@gmail.com>
2025-03-20 16:50:00 +0500

    Problems sloved

---
+++ Problems/Problem-0524/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Problem_0524;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var n = Console.ReadLine();
+        for(int i = 0; i < n.Length; i++)
+        {
+           Console.ReadLine();
+        }
+
+        Console.WriteLine(n);
+    }
+}
---
+++ Problems/Problem-0550/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem_0550;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var arrNums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        if (arrNums[0] >= arrNums[1])
+        {
+            Console.WriteLine(Math.Ceiling((arrNums[0] - arrNums[1])/10));
+        }
+        else if (arrNums[0] <= arrNums[1])
+        {
+            Console.WriteLine(Math.Ceiling((arrNums[1] - arrNums[0])/10));
+        }
+    }
+}
