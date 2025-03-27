63566a5fe108867ac28e103bf11ea30443e2c851
javohirtohirovich <javoergashev30@gmail.com>
2025-03-27 14:50:59 +0500

    Problems Sloved

---
+++ Problems/Problem-0562/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Problem_0562;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var stones = Console.ReadLine().Split().Select(long.Parse).ToArray();
+        var piledUpStones = Console.ReadLine().Split().Select(long.Parse).OrderByDescending(x=>x).ToArray();
+        var s = 0l;
+        if(piledUpStones.Length == 0)
+        {
+            Console.WriteLine(-1);
+            return;
+        }
+        for (int i = 0; i < piledUpStones.Length; i++)
+        {
+            s += piledUpStones[i];
+            if (s >= stones[0])
+            {
+                Console.WriteLine(i + 1);
+                return;
+            }
+        }
+        Console.WriteLine(-1);
+    }
+}
---
+++ Problems/Problem-0569/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Problem_0569;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var str = Console.ReadLine();
+        if(str.Contains("P" as string) || str.Contains("Q" as string) || str.Contains("7" as string))
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
